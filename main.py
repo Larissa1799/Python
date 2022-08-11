a104debc99815a75e53a6220528753ddd4347ec1
LARISSA MATTOS DE MARTINI <109092221+Larissa1799@users.noreply.github.com>
2022-08-11 12:13:49 +0000

    modificacao main

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-nota_aluno1 = 10.0
-nota_aluno2 = 5.0
-nota_aluno3 = 7.0
-nota_aluno4 = 2.5
-qtd_alunos = 4
-soma_alunos = nota_aluno1 + nota_aluno2 + nota_aluno3 + nota_aluno4
-media = soma_alunos / qtd_alunos
-print(media)
+descontos = 300.00
+salario_fixo = 1800.00
+vendas_mes = 240
+valor_comissao = 0.2
+valor_total_vendas = vendas_mes * valor_comissao
+valor_salario_liquido = (salario_fixo + valor_total_vendas) - descontos
+
+
+print(valor_salario_liquido)
---
+++ pythonmain.py
@@ -0,0 +1,8 @@
+nota_aluno1 = 10.0
+nota_aluno2 = 5.0
+nota_aluno3 = 7.0
+nota_aluno4 = 2.5
+qtd_alunos = 4
+soma_alunos = nota_aluno1 + nota_aluno2 + nota_aluno3 + nota_aluno4
+media = soma_alunos / qtd_alunos
+print(media)
